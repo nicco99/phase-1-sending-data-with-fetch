837c1b6fa802b9cf9e748850d372443f61df2e48
Nicholas Njeru <nicholasnjeru917@gmail.com>
2022-08-28 19:26:35 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Add your code here
+function submitData(str1, str2) {
+    let user = {name: str1,email:str2};
+    
+let objData = {
+    method: "POST",
+    headers: {
+        "content-type":"application/json",
+        Accept:"application/json" 
+    },
+    body: JSON.stringify(user)
+}
+
+return fetch("http://localhost:3000/users", objData)
+.then(response=> response.json())
+.then(serverResponse => {
+    return document.body.innerHTML = serverResponse.id
+})
+.catch(error=>document.body.innerHTML=error.message)
+}
+
+
+submitData("nicco","nicholasnjeru@gmail.com")
